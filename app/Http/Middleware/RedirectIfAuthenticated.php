cdc2385c2826d5fdf193da0eaad3e97bcd270e46
Sergey <52923458+Sergey98Am@users.noreply.github.com>
2020-07-10 18:07:12 +0400

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,25 +1,27 @@
 <?php
 
-// namespace App\Http\Middleware;
+namespace App\Http\Middleware;
 
-// use App\Providers\RouteServiceProvider;
-// use Closure;
-// use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Support\Facades\Auth;
 
-// class RedirectIfAuthenticated
-// {
-//     /**
-//      * Handle an incoming request.
-//      *
-//      * @param  \Illuminate\Http\Request  $request
-//      * @param  \Closure  $next
-//      * @param  string|null  $guard
-//      * @return mixed
-//      */
-//     public function handle($request, Closure $next, $guard = null)
-//     {
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check()) {
+            return redirect(RouteServiceProvider::HOME);
+        }
 
-
-//         return $next($request);
-//     }
-// }
+        return $next($request);
+    }
+}
